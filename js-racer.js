60e94cf6a3c98a1585ef75b0e4c998ca27ac9ab1
Dody Herawan <dody@Dodys-MacBook-Pro.local>
2018-09-06 10:11:33 +0700

    Release 0

--- js-racer.js
+++ js-racer.js
@@ -1,8 +1,11 @@
 "use strict"
 
+//randomized
 function diceRoll () {
-
+  return Math.round(Math.random() * 6);
 }
+
+console.log('diceRoll: ' + diceRoll());
 
 function sleep (milliseconds) {
   var start = new Date().getTime();
@@ -11,12 +14,27 @@ function sleep (milliseconds) {
       break;
     }
   }
-}
 
+}
+console.log('the duration: ' + sleep(4000))
+
+//empty board
 function printBoard () {
+  let col = process.argv[2];
+  let row = process.argv[3];
+  let fullRow = '';
 
+  for (let i = 0; i < row; i++) {
+    fullRow += '| ';
+  }
+
+  for (let c = 0; c < col; c++) {
+    console.log(fullRow)
+  }
 }
+printBoard()
 
+//when the player is there
 function printLine (player, pos) {
 
 }
