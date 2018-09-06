a656f4375530d52439bdfe1bb51a9d4b69c3569c
anastasia <anastasia.santoso.id@gmail.com>
2018-09-06 14:47:58 +0700

    Release 1

--- js-racer.js
+++ js-racer.js
@@ -1,15 +1,25 @@
 "use strict"
 
-const numOfPlayer = +process.argv[2]
-const panjangLintasan = +process.argv[3]
+const numOfPlayer = +process.argv[2];
+const panjangLintasan = +process.argv[3];
+
+let nameOfPlayer = 'abcdefghijklmnopqrstuvwxyz';
+let playerDetails = [];
+
+for (let i = 0; i < numOfPlayer; i++) {
+  let playerNameObj = {};
+  playerDetails.push(playerNameObj);
+  playerDetails[i]['name'] = nameOfPlayer[i];
+  playerDetails[i]['position'] = 0;
+}
 
 function diceRoll () {
-  return Math.floor(Math.random() * 6)
+  return Math.floor(Math.random() * (6-1) + 1);
 }
 
 function sleep (milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
+  let start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
     if ((new Date().getTime() - start) > milliseconds) {
       break;
     }
@@ -17,41 +27,48 @@ function sleep (milliseconds) {
 }
 
 function printBoard () {
-  let board = []
-  for (var i = 0; i < numOfPlayer; i++) {
-    board.push(printLine(nameOfPlayer[i], 0));
+  let board = [];
+  if (finished() === false) {
+    for (let i = 0; i < numOfPlayer; i++) {
+      advance(i);
+      board.push(printLine(playerDetails[i]['name'], playerDetails[i]['position']));
+    }
   }
-}
-
-let nameOfPlayer = 'abcdefghij'
-let playerNameArr = []
-for (var i = 0; i < numOfPlayer; i++) {
-  playerNameArr.push(nameOfPlayer[i]);
-  console.log(playerNameArr);
 }
 
 function printLine (player, pos) {
   let perLine = [];
-  for (var i = 0; i < panjangLintasan + 1; i++) {
+  for (let i = 0; i < panjangLintasan + 1; i++) {
     if (pos === i) {
-      perLine.push(' ' + player + ' ')
+      perLine.push(' ' + player + ' ');
     } else {
-      perLine.push('    ')
+      perLine.push('    ');
     }
   }
-  console.log(perLine.join('|'))
+  console.log(perLine.join('|'));
 }
 
-function advance (player) {
-
+function advance (lineNum) {
+    let moves = diceRoll();
+    if ((playerDetails[lineNum]['position'] + moves) >= panjangLintasan) {
+      playerDetails[lineNum]['position'] = panjangLintasan;
+    } else {
+      playerDetails[lineNum]['position'] += moves;
+    }
 }
 
 function finished () {
-
+  for (var i = 0; i < numOfPlayer; i++) {
+    if (playerDetails[i]['position'] >= panjangLintasan) {
+      winner(playerDetails[i]['name']);
+      return true;
+    }
+  }
+  return false;
 }
 
-function winner () {
-
+function winner (win) {
+  console.log('Player ' + win + ' is the winner!');
 }
 
 function clearScreen () {
@@ -60,6 +77,14 @@ function clearScreen () {
   console.clear();
 }
 
-console.log(process.argv);
-printBoard();
-//printLine('a', 5);
+if (numOfPlayer < 2) {
+  console.log('Minimal player = 2');
+} else if (panjangLintasan > 15 || panjangLintasan < 0) {
+  console.log('Maximum Panjang Lintasan = 15');
+} else {
+  while(finished() === false) {
+    sleep(800);
+    clearScreen();
+    printBoard();
+  }
+}
